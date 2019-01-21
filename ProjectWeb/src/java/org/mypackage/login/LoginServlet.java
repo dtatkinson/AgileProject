fd351a454971f45f37d442f8e5a2617b816d10dc
oliversimpson <oliversimpson@labpc161.computing.dundee.ac.uk>
2019-01-21 15:02:23 +0000

    Cleaned up class so it matches with test

--- ProjectWeb/src/java/org/mypackage/login/LoginServlet.java
+++ ProjectWeb/src/java/org/mypackage/login/LoginServlet.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package org.mypackage.login;
 
 import Database.DatabaseHandling;
@@ -15,14 +11,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
 /**
- *
  * @author lenardgaunt
  */
 @WebServlet(name = "LoginServlet", urlPatterns = {"/LoginServlet"})
 public class LoginServlet extends HttpServlet {
-
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
      * methods.
@@ -36,15 +29,14 @@ public class LoginServlet extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
+            
+        out.println("<!DOCTYPE html>");
             
         String username = request.getParameter("staffID");
         String password = request.getParameter("passwd");
         
         System.out.println("username: " + username);
         System.out.println("password: " + password);
-        
         
         DatabaseHandling conn = new DatabaseHandling();
         
@@ -90,16 +82,13 @@ public class LoginServlet extends HttpServlet {
             
             if(flag == false){
                 out.print("Wrong credentials, idiot");
-            }
-            
+            }   
         }
-        catch (Exception e){
-            
+        catch (Exception e){    
         }
         
-        }
-        
-    }
+    }  
+}
     public boolean processUsername(String data, String username){
         
         if (data.equals(username)){
@@ -114,67 +103,5 @@ public class LoginServlet extends HttpServlet {
         }
         return false;
     }
-    
-    public void redirectToDash(String role){
-        
-    }
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-        
-        String username = request.getParameter("staffID");
-        String password = request.getParameter("passwd");
-        
-        System.out.println("username: " + username);
-        System.out.println("password: " + password);
- 
-        // do some processing here...
-         
-        // get response writer
-        PrintWriter writer = response.getWriter();
-         
-        // build HTML code
-        String htmlRespone = "<html>";
-        htmlRespone += "<h2>Your username is: " + username + "<br/>";      
-        htmlRespone += "Your password is: " + password + "</h2>";    
-        htmlRespone += "</html>";
-         
-        // return response
-        writer.println(htmlRespone);
-    }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
 
 }
