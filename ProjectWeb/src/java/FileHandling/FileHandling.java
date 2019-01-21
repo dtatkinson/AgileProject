be3ed85c749751eb2b1ce062a77c527dab1eb1d7
matthewmchale <matthewmchale@labpc164.computing.dundee.ac.uk>
2019-01-21 12:15:01 +0000

    add FileHandling  test Class

--- ProjectWeb/src/java/FileHandling/FileHandling.java
+++ ProjectWeb/src/java/FileHandling/FileHandling.java
@@ -6,9 +6,47 @@
 package FileHandling;
 
 /**
- *
+ * Class to handle all things related to files
  * @author matthewmchale
  */
 public class FileHandling {
+    
+    public void createFile(){
+        return;
+    }
+    
+    public void writeToFile(){
+        return;    
+    }
+    
+    public void appendToFile(){
+        return;
+    }
+    
+    public void checkIfFileExists(){
+        return;
+    }
+    
+    public void readFromFile(){
+        return;
+    }
+    
+    //to check what level of comments the user can read?
+    public void compareFiles(){
+        return;
+    }
+    
+    //download/upload functions will just 'move' files to/from the server
+    public void downloadFile(){
+        return;
+    }
+    
+    public void uploadFile(){
+        return;
+    }
+    
+    public void deleteFile(){
+        return;
+    }
     
 }
