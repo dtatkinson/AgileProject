90f26b7785e69042c1b2b46eeb354a2b861b1d3f
oliversimpson <oliversimpson@labpc161.computing.dundee.ac.uk>
2019-01-21 11:14:01 +0000

    Mostly finished methods and basic test classes Still need advanced tests

--- ProjectWeb/test/Database/DatabaseHandlingTest.java
+++ ProjectWeb/test/Database/DatabaseHandlingTest.java
@@ -12,8 +12,12 @@ import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
+import static org.junit.Assert.*;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
 
-
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 /**
  *
  * @author oliversimpson
@@ -48,151 +52,151 @@ public class DatabaseHandlingTest {
     
     
     @Test(expected = Test.None.class) //Test that a connection has been established i.e no exception thrown
-    public void testMain() throws Exception {
+    public void A_testMain() throws Exception {
         String[] args = null;
         DatabaseHandling.main(args);    
     }
     
     @Test //Tests that inserting into the database works
-    public void testAddStaff() throws Exception {
+    public void B_testAddStaff() throws Exception {
         System.out.println("Add staff");
         DatabaseHandling instance = new DatabaseHandling();
-        instance.addStaff(1,"Testname", "Testrole", "Testpswd");
+        instance.addStaff("Testname", "Testrole", "Testpswd");
     }
     
     @Test
-    public void testRemoveStaff() throws Exception {
+    public void C_testRemoveStaff() throws Exception {
         System.out.println("Remove Staff");
         DatabaseHandling instance = new DatabaseHandling();
-        instance.addStaff(2,"Testname", "Testrole", "Testpswd");
-        instance.removeStaff(1);
+        instance.addStaff("Testname", "Testrole", "Testpswd");
+        instance.removeStaff(2);
     }
 
     @Test
-    public void testEditStaffName() throws Exception {
+    public void D_testEditStaffName() throws Exception {
         System.out.println("Edit staff Name");
         DatabaseHandling instance = new DatabaseHandling();
-        instance.addStaff(3,"Testname", "Testrole", "Testpswd");
+        instance.addStaff("Testname", "Testrole", "Testpswd");
         instance.editStaffName(3,"Sausage");
     }
 
     @Test
-    public void testEditStaffRole() throws Exception {
+    public void E_testEditStaffRole() throws Exception {
         System.out.println("Edit staff Role");
         DatabaseHandling instance = new DatabaseHandling();
-        instance.addStaff(4,"Testname", "Testrole", "Testpswd");
+        instance.addStaff("Testname", "Testrole", "Testpswd");
         instance.editStaffRole(4,"Pie");
     }
 
     @Test
-    public void testEditStaffPassword() throws Exception {
+    public void F_testEditStaffPassword() throws Exception {
         System.out.println("Edit staff Password");
         DatabaseHandling instance = new DatabaseHandling();
-        instance.addStaff(4,"Testname", "Testrole", "Testpswd");
+        instance.addStaff("Testname", "Testrole", "Testpswd");
         instance.editStaffPassword(5,"Fish");
     }
 
     @Test
-    public void testCreateExam() throws Exception {
+    public void G_testCreateExam() throws Exception {
         System.out.println("Create Exam");
         DatabaseHandling instance = new DatabaseHandling();
-        instance.createExam(1,"Comp","AC130","Scumdee","Biz","1984");
+        instance.createExam("Comp","AC130","Scumdee","Biz","1984");
     }
 
     @Test
-    public void testDeleteExam() throws Exception {
+    public void H_testDeleteExam() throws Exception {
         System.out.println("Delete Exam");
         DatabaseHandling instance = new DatabaseHandling();
-        instance.createExam(2,"Comp","AC130","Scumdee","Biz","1984");
+        instance.createExam("Comp","AC130","Scumdee","Biz","1984");
         instance.deleteExam(2);
     }
 
     @Test
-    public void testEditExamStatus() throws Exception {
+    public void I_testEditExamStatus() throws Exception {
         System.out.println("Edit Exam Status");
         DatabaseHandling instance = new DatabaseHandling();
-        instance.createExam(3,"Comp","AC130","Scumdee","Biz","1984");
+        instance.createExam("Comp","AC130","Scumdee","Biz","1984");
         instance.editExamStatus(3,"in progress");
     }
 
 
     @Test
-    public void testEditExamModuleName() throws Exception {
+    public void J_testEditExamModuleName() throws Exception {
         System.out.println("Edit Exam Name");
         DatabaseHandling instance = new DatabaseHandling();
-        instance.createExam(4,"Comp","AC130","Scumdee","Biz","1984");
+        instance.createExam("Comp","AC130","Scumdee","Biz","1984");
         instance.editExamModuleName(4,"Biznes");
     }
 
     @Test
-    public void testEditExamModuleCode() throws Exception {
+    public void K_testEditExamModuleCode() throws Exception {
         System.out.println("Edit Exam Code");
         DatabaseHandling instance = new DatabaseHandling();
-        instance.createExam(5,"Comp","AC130","Scumdee","Biz","1984");
+        instance.createExam("Comp","AC130","Scumdee","Biz","1984");
         instance.editExamModuleCode(5,"AC12");
     }
 
     @Test
-    public void testEditExamInstitution() throws Exception {
+    public void L_testEditExamInstitution() throws Exception {
         System.out.println("Edit Exam Institution");
         DatabaseHandling instance = new DatabaseHandling();
-        instance.createExam(6,"Comp","AC130","Scumdee","Biz","1984");
+        instance.createExam("Comp","AC130","Scumdee","Biz","1984");
         instance.editExamInstitution(6,"Dundee High");
     }
 
     @Test
-    public void testEditExamSchool() throws Exception {
+    public void M_testEditExamSchool() throws Exception {
         System.out.println("Edit Exam School");
         DatabaseHandling instance = new DatabaseHandling();
-        instance.createExam(7,"Comp","AC130","Scumdee","Biz","1984");
+        instance.createExam("Comp","AC130","Scumdee","Biz","1984");
         instance.editExamSchool(7,"Arts and crafts");
     }
     
     @Test
-    public void testEditExamAcedmeicYear() throws Exception {
+    public void N_testEditExamAcedmeicYear() throws Exception {
         System.out.println("Edit Exam Year");
         DatabaseHandling instance = new DatabaseHandling();
-        instance.createExam(8,"Comp","AC130","Scumdee","Biz","1984");
+        instance.createExam("Comp","AC130","Scumdee","Biz","1984");
         instance.editExamAcademicYear(8,"2019");
     }
 
     @Test
-    public void testInternalSignExam() throws Exception {
+    public void O_testInternalSignExam() throws Exception {
         System.out.println("Internal Exam signed");
         DatabaseHandling instance = new DatabaseHandling();
-        instance.createExam(9,"Comp","AC130","Scumdee","Biz","1984");
+        instance.createExam("Comp","AC130","Scumdee","Biz","1984");
         instance.internalSignExam(9,2);
     }
 
     @Test
-    public void testInternalDateEdit() throws Exception {
-  
+    public void P_testInternalDateEdit() throws Exception {
+        fail("not done");
     }
 
     @Test
-    public void testExamCommiteeSignExam() throws Exception {
+    public void Q_testExamCommiteeSignExam() throws Exception {
         System.out.println("Commitee Exam signed");
         DatabaseHandling instance = new DatabaseHandling();
-        instance.createExam(10,"Comp","AC130","Scumdee","Biz","1984");
+        instance.createExam("Comp","AC130","Scumdee","Biz","1984");
         instance.examCommiteeSignExam(10,2);
     }
 
     @Test
-    public void testExamCommiteeDateEdit() throws Exception {
-        
+    public void R_testExamCommiteeDateEdit() throws Exception {
+        fail("not done");
     }
 
     @Test
-    public void testExternalSignExam() throws Exception {
+    public void S_testExternalSignExam() throws Exception {
         System.out.println("External Exam signed");
         DatabaseHandling instance = new DatabaseHandling();
-        instance.createExam(11,"Comp","AC130","Scumdee","Biz","1984");
-        instance.examCommiteeSignExam(11,2);
+        instance.createExam("Comp","AC130","Scumdee","Biz","1984");
+        instance.externalSignExam(11,2);
     }
 
     @Test
-    public void testExternalDateEdit() throws Exception {
-       
+    public void T_testExternalDateEdit() throws Exception {
+       fail("not done");
     }
     /*
     @Test
