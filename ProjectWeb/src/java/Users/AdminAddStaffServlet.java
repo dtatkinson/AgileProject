cb5c9c666b4b4ab1da4df11304e8d498cab88d6e
oliversimpson <oliversimpson@labpc161.computing.dundee.ac.uk>
2019-01-22 14:06:51 +0000

    Renamed AdminServlet to addstaffservelet

--- ProjectWeb/src/java/Users/AdminAddStaffServlet.java
+++ ProjectWeb/src/java/Users/AdminAddStaffServlet.java
@@ -20,7 +20,7 @@ import javax.servlet.http.HttpServletResponse;
  * @author oliversimpson
  */
 @WebServlet(name = "AdminServlet", urlPatterns = {"/AdminServlet"})
-public class AdminServlet extends HttpServlet {
+public class AdminAddStaffServlet extends HttpServlet {
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
