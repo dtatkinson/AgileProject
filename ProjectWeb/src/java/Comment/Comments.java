69c0c5e78ae235c2f24e01ffc0ba71c9a1e321b0
matthewmchale <matthewmchale@labpc164.computing.dundee.ac.uk>
2019-01-24 13:14:02 +0000

    added ability to write acknowledgements for comments, as well as a read method to check all comments have an acknowledgement(primitive)

--- ProjectWeb/src/java/Comment/Comments.java
+++ ProjectWeb/src/java/Comment/Comments.java
@@ -20,23 +20,24 @@ public class Comments {
         String commentFile = "comments.txt" ;
         String path = modCode + "\\2019\\";
         String fullPath = path+commentFile;
+        String commentR = "c." + comment;
         
         if(instance.checkIfFileExists(fullPath))
         {         
-            instance.appendToFile(path, commentFile, comment);
+            instance.appendToFile(path, commentFile, commentR);
             instance.appendToFile(path, commentFile, signBy);
         }else if(!instance.checkIfFileExists(fullPath))
         {
             instance.createFile(path, commentFile);
-            instance.writeToFile(path, commentFile, comment);
+            instance.writeToFile(path, commentFile, commentR);
             instance.appendToFile(path, commentFile, signBy);
         }
-        //need to create servlet to get session, and hence get the username, to get access rights or somethign please send big HELP
+        
     }
-    /*public void readComment(String modCode) throws IOException{
-        String[] comment;
+    public boolean readComment(String modCode) throws IOException{
+        String[] comment = null;
         FileHandling instance = new FileHandling();
-        String commentFile = "comment.txt" ;
+        String commentFile = "comments.txt" ;
         String path = modCode + "\\2019\\";
         String fullPath = path+commentFile;
         if(instance.checkIfFileExists(fullPath))
@@ -44,7 +45,42 @@ public class Comments {
             comment = instance.readFromFile(path, commentFile);
             System.out.println(Arrays.toString(comment));
         }
-    **/
+        int cmmt = 0;
+        int ack = 0;
+        String line;
+        for(int i = 0; i < comment.length; i++)
+        {
+           line = Character.toString(comment[i].charAt(0));
+           if(line.equals("c")){
+               cmmt++;
+           }else if (line.equals("a"))
+           {
+               ack++;
+           }
+        }
+        if(cmmt == ack){
+            return true;
+        }
+        return false;
+    }
+    
+    public void ackComments(String modCode,String comment,String signBy) throws IOException{
+        FileHandling instance = new FileHandling();
+        String commentFile = "comments.txt" ;
+        String path = modCode + "\\2019\\";
+        String fullPath = path+commentFile;
+        String commentR = "a." + comment;
         
+        if(instance.checkIfFileExists(fullPath))
+        {         
+            instance.appendToFile(path, commentFile, commentR);
+            instance.appendToFile(path, commentFile, signBy);
+        }else if(!instance.checkIfFileExists(fullPath))
+        {
+            instance.createFile(path, commentFile);
+            instance.writeToFile(path, commentFile, commentR);
+            instance.appendToFile(path, commentFile, signBy);
+        }
+    }
 }
 
