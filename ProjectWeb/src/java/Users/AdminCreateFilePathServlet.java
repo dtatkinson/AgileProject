7b72c56faf1f17a64f115fb3f8fdc34537549b28
matthewmchale <matthewmchale@labpc164.computing.dundee.ac.uk>
2019-01-24 17:48:28 +0000

    removed some unused imports

--- ProjectWeb/src/java/Users/AdminCreateFilePathServlet.java
+++ ProjectWeb/src/java/Users/AdminCreateFilePathServlet.java
@@ -8,14 +8,12 @@ package Users;
 import Database.DatabaseHandling;
 import FileHandling.FileHandling;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.sql.ResultSet;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import jdk.nashorn.internal.objects.NativeString;
 
 /**
  *
