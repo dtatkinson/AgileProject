d51e4925d3f41a832f38e5ffa9aeb5925226e32c
camerontaylor <camerontaylor@labpc144.computing.dundee.ac.uk>
2019-01-26 17:00:12 +0000

    Table Changes

--- ProjectWeb/src/java/Users/AdminViewStaffServlet.java
+++ ProjectWeb/src/java/Users/AdminViewStaffServlet.java
@@ -43,17 +43,27 @@ public class AdminViewStaffServlet extends HttpServlet {
             try{
                 ResultSet staffList  = conn.searchTable(search, "Staff", "StaffName");
                 out.println("<HTML>");
+                out.println("<head>");
+                out.println("<!-- Latest compiled and minified CSS -->\n" +
+"<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css\" integrity=\"sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u\" crossorigin=\"anonymous\">");
+                out.println("</head>");
                 out.println("<BODY>");
 
-                out.println("<table style='width:100%' border='1'");
+                out.println( "<div class ='img'>");
+                out.println("<a href='StaffDashboard.jsp'>");
+                out.println("<img src='DundeeUniLogo.png' width='100' height='125' alt='DundeeLogo' vertical-align='center' ></img>");
+                out.println("</a>");
+                out.println("</div>");
+   
+               out.println("<table style='width: 50%' border='4' align='center' >");
 
                 out.println("<tr>");
-                    out.println("<th>");
-                    out.println("Staff Username");
-                    out.println("</th>");
-                    out.println("<th>");
-                    out.println("Staff Role");
-                    out.println("</th>");
+                out.println("<th>");
+                out.println("Staff Username");
+                out.println("</th>");
+                out.println("<th>");
+                out.println("Staff Role");
+                out.println("</th>");
                 out.println("</tr>");
                         
                 while(staffList.next()){
@@ -76,7 +86,10 @@ public class AdminViewStaffServlet extends HttpServlet {
                 out.println("</table>");
                 
                 out.println("<form action='ManagementPage.jsp'>");
-                    out.println("<input type='submit' value='Back'>");
+                  out.println("<br>");
+                out.println("<div align='center'>");
+                out.println("<input type='submit' value='Back'>");
+                out.println("</div>");
                 out.println("</form>");
                 
                 out.println("</BODY>");
